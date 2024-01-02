5aec83a18898bbdaf260b56a09e8291b4e9f4a09
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2024-01-02 17:37:28 -0300

    Implementando o Controller de Produtos.

---
+++ GeekShooping.Web/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.FindAll();
+
+            return View(products);
+        }
+    }
+}
