3bfb82938bfa4ffbbbc9f4ed091c94695904116d
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2024-01-03 15:16:29 -0300

    Implementando a classe IdentityConfiguration.

---
+++ GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer;
+
+namespace GeekShopping.IdentityServer.Configuration
+{
+    public static class IdentityConfiguration
+    {
+        public const string Admin = "Admin";
+        public const string Client = "Client";
+        public const string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("geek_shopping", "GeekShopping Server"),
+                new ApiScope(name: "read", "Read data."),
+                new ApiScope(name: "write", "Write data."),
+                new ApiScope(name: "delete", "Delete data."),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("my_super_secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"read", "write", "profile" }
+                },
+                new Client
+                {
+                    ClientId = "geek_shopping",
+                    ClientSecrets = { new Secret("my_super_secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = {"http://localhost:5148/signin-oidc"},
+                    PostLogoutRedirectUris = {"http://localhost:5148/signout-callback-oidc"},
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "geek_shopping"
+                    }
+                }
+            };
+    }
+}
