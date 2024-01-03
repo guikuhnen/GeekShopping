539968bd824a060b05b4a7bfb0945927427a3d56
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2024-01-03 11:48:45 -0300

    Adição do DbContext ao Identity Server.

---
+++ GeekShopping.IdentityServer/DbModel/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GeekShopping.IdentityServer.DbModel
+{
+    public class ApplicationUser : IdentityUser
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+    }
+}
