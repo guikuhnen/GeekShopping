7522fafe1f83089ca3dd5a26597e3555e9a9f6f1
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2024-01-03 15:24:40 -0300

    Adicionando Configurações de Banco de Dados e Segurança.

--- GeekShopping.IdentityServer/Program.cs
+++ GeekShopping.IdentityServer/Program.cs
@@ -1,3 +1,11 @@
+using Duende.IdentityServer.AspNetIdentity;
+using Duende.IdentityServer.Services;
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.DbModel;
+using GeekShopping.IdentityServer.DbModel.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace GeekShopping.IdentityServer
 {
     public class Program
@@ -7,6 +15,29 @@ namespace GeekShopping.IdentityServer
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<MySQLContext>(options =>
+                options.UseMySql(
+                    builder.Configuration["MySQLConnection:MySQLConnectionString"],
+                    new MySqlServerVersion(new Version(8, 0, 35))));
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<MySQLContext>()
+                .AddDefaultTokenProviders();
+
+            var identityServerBuilder = builder.Services.AddIdentityServer(options =>
+                {
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseSuccessEvents = true;
+                    options.EmitStaticAudienceClaim = true;
+                }).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+                    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+                    .AddInMemoryClients(IdentityConfiguration.Clients)
+                    .AddAspNetIdentity<ApplicationUser>();
+
+            identityServerBuilder.AddDeveloperSigningCredential();
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
@@ -19,7 +50,7 @@ namespace GeekShopping.IdentityServer
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseIdentityServer();
             app.UseAuthorization();
 
             app.MapControllerRoute(
