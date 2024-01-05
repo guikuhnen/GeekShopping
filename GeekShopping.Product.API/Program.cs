a3f265369d27c5cbdd2bd0790c4e003b5ce6e482
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2024-01-05 15:47:24 -0300

    Adicionando Autenticação ao Swagger (Open API).

--- GeekShopping.Product.API/Program.cs
+++ GeekShopping.Product.API/Program.cs
@@ -8,35 +8,35 @@ using Microsoft.OpenApi.Models;
 
 namespace GeekShopping.ProductAPI
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			builder.Services.AddDbContext<MySQLContext>(options =>
-				options.UseMySql(
-					builder.Configuration["MySQLConnection:MySQLConnectionString"],
-					new MySqlServerVersion(new Version(8, 0, 35))));
+            // Add services to the container.
+            builder.Services.AddDbContext<MySQLContext>(options =>
+                options.UseMySql(
+                    builder.Configuration["MySQLConnection:MySQLConnectionString"],
+                    new MySqlServerVersion(new Version(8, 0, 35))));
 
-			// AutoMapper
-			IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
-			builder.Services.AddSingleton(mapper);
-			builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            // AutoMapper
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            builder.Services.AddSingleton(mapper);
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-			builder.Services.AddScoped<IProductRepository, ProductRepository>();
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
-			builder.Services.AddControllers();
+            builder.Services.AddControllers();
 
-			builder.Services.AddAuthentication("Bearer")
-				.AddJwtBearer(options =>
-				{
-					options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
-					options.TokenValidationParameters = new TokenValidationParameters()
-					{
-						ValidateAudience = false
-					};
+            builder.Services.AddAuthentication("Bearer")
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateAudience = false
+                    };
                 });
 
             builder.Services.AddAuthorization(options =>
@@ -50,27 +50,53 @@ namespace GeekShopping.ProductAPI
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
-			});
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
+                options.EnableAnnotations();
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"Enter 'Bearer' [space] and your token!",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In= ParameterLocation.Header
+                        },
+                        new List<string> ()
+                    }
+                });
+            });
 
-			var app = builder.Build();
+            var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
             app.UseHttpsRedirection();
 
             app.UseAuthentication();
             app.UseAuthorization();
 
-			app.MapControllers();
+            app.MapControllers();
 
-			app.Run();
-		}
-	}
+            app.Run();
+        }
+    }
 }
