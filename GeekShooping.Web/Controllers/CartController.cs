26dd295731172a3e7fb7ad5e79d8093e4c7c69b6
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2024-01-10 10:49:42 -0300

    Refatorando o CartController para Recuperar o Cupom e Aplicar Desconto.

--- GeekShooping.Web/Controllers/CartController.cs
+++ GeekShooping.Web/Controllers/CartController.cs
@@ -10,11 +10,13 @@ namespace GeekShopping.Web.Controllers
 	{
 		private readonly IProductService _productService;
 		private readonly ICartService _cartService;
+		private readonly ICouponService _couponService;
 
-		public CartController(IProductService productService, ICartService cartService)
+		public CartController(IProductService productService, ICartService cartService, ICouponService couponService)
 		{
 			_productService = productService ?? throw new ArgumentNullException(nameof(productService));
 			_cartService = cartService ?? throw new ArgumentNullException(nameof(productService));
+			_couponService = couponService ?? throw new ArgumentNullException(nameof(productService));
 		}
 
 		[Authorize]
@@ -31,8 +33,20 @@ namespace GeekShopping.Web.Controllers
 			var response = await _cartService.FindCartByUserId(userId, token);
 
 			if (response?.CartHeader != null)
+			{
+				if (!string.IsNullOrEmpty(response.CartHeader.CouponCode))
+				{
+					var coupon = await _couponService.GetCoupon(response.CartHeader.CouponCode, token);
+
+					if (coupon?.CouponCode != null)
+						response.CartHeader.DiscountAmount = coupon.DiscountAmount;
+				}
+
 				foreach (var detail in response.CartDetails)
 					response.CartHeader.PurchaseAmount += (detail.Product.Price * detail.Count);
+
+				response.CartHeader.PurchaseAmount -= response.CartHeader.DiscountAmount;
+			}
 
 			return response;
 		}
--- GeekShooping.Web/Models/CartHeaderViewModel.cs
+++ GeekShooping.Web/Models/CartHeaderViewModel.cs
@@ -9,5 +9,25 @@
 		public string CouponCode { get; set;}
 
 		public decimal PurchaseAmount { get; set; }
+
+		// Checkout
+
+		public decimal DiscountAmount { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public DateTime DateTime { get; set; }
+
+		public string Phone { get; set; }
+
+		public string Email { get; set; }
+
+		public string CardNumber { get; set; }
+
+		public string CVV { get; set; }
+
+		public string ExpiryMonthYear { get; set; }
 	}
 }
