7172e2c79163aac9f3c255932db659979bd86db2
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2024-01-10 17:43:48 -0300

    Adicionando o RabbitMQ ao Projeto e Implementando o RabbitMQMessageSender.

---
+++ GeekShopping.CartAPI/RabbitMQSender/RabbitMQMessageSender.cs
@@ -0,0 +1,59 @@
+﻿using GeekShopping.CartAPI.Messages;
+using GeekShopping.MessageBus;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace GeekShopping.CartAPI.RabbitMQSender
+{
+	public class RabbitMQMessageSender : IRabbitMQMessageSender
+	{
+		private readonly string _hostName;
+		private readonly string _userName;
+		private readonly string _password;
+		private readonly IConfiguration _config;
+		private IConnection _connection;
+
+		public RabbitMQMessageSender(IConfiguration config)
+		{
+			_config = config;
+
+			_hostName = _config.GetValue<string>("RabbitMQ:HostName");
+			_userName = _config.GetValue<string>("RabbitMQ:UserName");
+			_password = _config.GetValue<string>("RabbitMQ:Password");
+		}
+
+		public void SendMessage(BaseMessage baseMessage, string queueName)
+		{
+			var factory = new ConnectionFactory
+			{
+				HostName = _hostName,
+				UserName = _userName,
+				Password = _password
+			};
+
+			_connection = factory.CreateConnection();
+
+			using var channel = _connection.CreateModel();
+			{
+				channel.QueueDeclare(queue: queueName, false, false, false, arguments: null);
+
+				byte[] body = GetMessageAsByteArray(baseMessage);
+
+				channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+			}
+		}
+
+		private byte[] GetMessageAsByteArray(BaseMessage baseMessage)
+		{
+			var options = new JsonSerializerOptions
+			{
+				WriteIndented = true
+			};
+
+			string json = JsonSerializer.Serialize<CheckoutHeaderVO>((CheckoutHeaderVO)baseMessage, options);
+
+			return Encoding.UTF8.GetBytes(json);
+		}
+	}
+}
