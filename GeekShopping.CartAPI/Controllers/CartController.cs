88766a38cf3c2d202cb8b27e8386f3235fbe2dd2
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2024-01-11 15:59:36 -0300

    Ajustando o CartController para Validar Alterações no Coupon.

--- GeekShopping.CartAPI/Controllers/CartController.cs
+++ GeekShopping.CartAPI/Controllers/CartController.cs
@@ -2,6 +2,7 @@
 using GeekShopping.CartAPI.Messages;
 using GeekShopping.CartAPI.RabbitMQSender;
 using GeekShopping.CartAPI.Repositories;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,19 +12,21 @@ namespace GeekShopping.CartAPI.Controllers
 	[ApiController]
 	public class CartController : ControllerBase
 	{
-		private ICartRepository _repository;
+		private ICartRepository _cartRepository;
+		private ICouponRepository _couponRepository;
 		private IRabbitMQMessageSender _messageSender;
 
-		public CartController(ICartRepository repository, IRabbitMQMessageSender messageSender)
+		public CartController(ICartRepository cartRepository, ICouponRepository couponRepository, IRabbitMQMessageSender messageSender)
 		{
-			_repository = repository ?? throw new ArgumentNullException(nameof(repository));
-			_messageSender = messageSender ?? throw new ArgumentNullException(nameof(repository));
+			_cartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
+			_couponRepository = couponRepository ?? throw new ArgumentNullException(nameof(couponRepository));
+			_messageSender = messageSender ?? throw new ArgumentNullException(nameof(messageSender));
 		}
 
 		[HttpPost("add-cart")]
 		public async Task<ActionResult<CartVO>> AddCart(CartVO cartVO)
 		{
-			var cart = await _repository.SaveOrUpdateCart(cartVO);
+			var cart = await _cartRepository.SaveOrUpdateCart(cartVO);
 
 			if (cart == null)
 				return NotFound();
@@ -37,12 +40,20 @@ namespace GeekShopping.CartAPI.Controllers
 			if (checkoutHeaderVO?.UserId == null)
 				return BadRequest();
 
-			var cart = await _repository.FindCartByUserId(checkoutHeaderVO.UserId);
+			var cart = await _cartRepository.FindCartByUserId(checkoutHeaderVO.UserId);
 
 			if (cart == null)
 				return NotFound();
 
+			if (!string.IsNullOrEmpty(checkoutHeaderVO.CouponCode))
+			{
+				string token = Request.Headers["Authorization"];
 
+				CouponVO coupon = await _couponRepository.GetCouponByCouponCode(checkoutHeaderVO.CouponCode, token);
+
+				if (checkoutHeaderVO.DiscountAmount != coupon.DiscountAmount)
+					return StatusCode(412);
+			}
 
 			checkoutHeaderVO.CartDetails = cart.CartDetails;
 			checkoutHeaderVO.DateTime = DateTime.Now.ToUniversalTime();
@@ -55,7 +66,7 @@ namespace GeekShopping.CartAPI.Controllers
 		[HttpGet("find-cart/{id}")]
 		public async Task<ActionResult<CartVO>> FindCartByUserId(string id)
 		{
-			var cart = await _repository.FindCartByUserId(id);
+			var cart = await _cartRepository.FindCartByUserId(id);
 
 			if (cart == null)
 				return NotFound();
@@ -66,7 +77,7 @@ namespace GeekShopping.CartAPI.Controllers
 		[HttpPut("update-cart")]
 		public async Task<ActionResult<CartVO>> UpdateCart(CartVO cartVO)
 		{
-			var cart = await _repository.SaveOrUpdateCart(cartVO);
+			var cart = await _cartRepository.SaveOrUpdateCart(cartVO);
 
 			if (cart == null)
 				return NotFound();
@@ -77,7 +88,7 @@ namespace GeekShopping.CartAPI.Controllers
 		[HttpPost("apply-coupon")]
 		public async Task<ActionResult<CartVO>> ApplyCoupon(CartVO cartVO)
 		{
-			var status = await _repository.ApplyCoupon(cartVO.CartHeader.UserId, cartVO.CartHeader.CouponCode);
+			var status = await _cartRepository.ApplyCoupon(cartVO.CartHeader.UserId, cartVO.CartHeader.CouponCode);
 
 			if (!status)
 				return NotFound();
@@ -88,7 +99,7 @@ namespace GeekShopping.CartAPI.Controllers
 		[HttpDelete("remove-coupon/{userId}")]
 		public async Task<ActionResult<CartVO>> RemoveCoupon(string userId)
 		{
-			var status = await _repository.RemoveCoupon(userId);
+			var status = await _cartRepository.RemoveCoupon(userId);
 
 			if (!status)
 				return NotFound();
@@ -99,7 +110,7 @@ namespace GeekShopping.CartAPI.Controllers
 		[HttpDelete("remove-cart/{id}")]
 		public async Task<ActionResult<CartVO>> RemoveCart(int id)
 		{
-			var status = await _repository.RemoveFromCart(id);
+			var status = await _cartRepository.RemoveFromCart(id);
 
 			if (!status)
 				return BadRequest();
