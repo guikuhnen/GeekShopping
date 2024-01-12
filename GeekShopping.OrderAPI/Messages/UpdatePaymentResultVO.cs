376f615288e2017057b72650f2d4fbd08c4f03d8
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2024-01-12 10:46:21 -0300

    Criação dos VO's Representando a Mensagem Enviada ao RabbitMQ.

---
+++ GeekShopping.OrderAPI/Messages/UpdatePaymentResultVO.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.MessageBus;
+
+namespace GeekShopping.OrderAPI.Messages
+{
+	public class UpdatePaymentResultVO : BaseMessage
+	{
+		public long OrderId { get; set; }
+
+		public bool Status{ get; set; }
+
+		public string Email { get; set; }
+	}
+}
